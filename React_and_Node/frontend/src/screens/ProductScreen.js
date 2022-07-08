20de30f6a157fc05dcce3882353ea4c880373f04
Barclaysdev2011 <shakilmustafiz17@gmail.com>
2022-07-08 15:17:58 -0400

    updated product screen page

--- React_and_Node/frontend/src/screens/ProductScreen.js
+++ React_and_Node/frontend/src/screens/ProductScreen.js
@@ -61,7 +61,7 @@ const ProductScreen = () => {
                     </ListGroup.Item>
 
                     <ListGroup.Item>
-                        <Button className='btn-block' type='button' disabled={product.countInStock==0}>Add To Cart</Button>
+                        <Button className='btn-block' type='button' disabled={product.countInStock===0}>Add To Cart</Button>
                     </ListGroup.Item>
                 </ListGroup>
             </Card>
