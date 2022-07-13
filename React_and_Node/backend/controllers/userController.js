2d9f2bf4bc952329850e89cf7d9fab5eae0b043d
Barclaysdev2011 <shakilmustafiz17@gmail.com>
2022-07-12 23:41:04 -0400

    login changes

--- React_and_Node/backend/controllers/userController.js
+++ React_and_Node/backend/controllers/userController.js
@@ -1,8 +1,9 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-
+import generatetoken from '../utils/generatetoken.js' 
+  
 const authUser =asyncHandler(async(req,res)=>{
-    console.log('hell')
+
     const {email,password } = req.body
     const user = await User.findOne({email})
     if(user && (await user.matchPassword(password))){
@@ -11,7 +12,7 @@ const authUser =asyncHandler(async(req,res)=>{
             name:user.name,
             email:user.email,
             isAdmin:user.isAdmin,
-            token:null
+            token:generatetoken(user._id)
         })
     }else{
         res.status(404)
@@ -19,4 +20,46 @@ const authUser =asyncHandler(async(req,res)=>{
     }
 })
 
-export {authUser}
+
+const registerUser =asyncHandler(async(req,res)=>{
+
+    const {name,email,password } = req.body
+    const userexist = await User.findOne({email})
+   if(userexist){
+    res.status(400)
+    throw new Error('user already exist')
+   }
+   const user = await User.create({
+    name,email,password
+   })
+   if(user){
+    res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token:generatetoken(user._id)
+    })
+   }else{
+    res.status(400)
+    throw new Error('user not found')
+}
+})
+
+const getUserProfile =asyncHandler(async(req,res)=>{
+
+    const user = await User.findById(req.body._id)
+     if(user){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin
+        })
+     }else{
+        res.status(404)
+        throw new Error('user not found')
+     }
+})
+
+export {authUser,getUserProfile,registerUser}
--- React_and_Node/backend/routes/userRoutes.js
+++ React_and_Node/backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
-
+import { authUser, getUserProfile ,registerUser} from "../controllers/userController.js";
+import {protect} from '../middleware/authMiddleware.js'
 const router = express.Router()
 router.post('/login',authUser)
-
+router.route('/profile').get(protect,getUserProfile)
+router.post('/users',registerUser)
 export default router
